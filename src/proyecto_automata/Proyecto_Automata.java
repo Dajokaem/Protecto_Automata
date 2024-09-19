184d5825fb1c186f8154b4a98cb86c7d0bbe0f3c
Dajokaem <jos562374@gmail.com>
2024-09-19 16:25:24 -0600

    Primer commit

---
+++ src/proyecto_automata/Proyecto_Automata.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_automata;
+
+import GUI.Main;
+
+/**
+ *
+ * @author jos56
+ */
+public class Proyecto_Automata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main m = new Main();
+        m.show();
+        
+        
+    }
+    
+}
